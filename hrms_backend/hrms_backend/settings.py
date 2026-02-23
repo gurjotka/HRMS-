0f3fe2786db32b42353a804b1a6b488ecc09bb1d
gurjot <gurjotkaur358@gmail.com>
2026-02-23 20:51:00 +0530

    settings update commit

--- hrms_backend/hrms_backend/settings.py
+++ hrms_backend/hrms_backend/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g=h)pqsfi-s0n=i5c#30*+*w8vtig!^n*2mk_3^v+dh%ubyafw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
